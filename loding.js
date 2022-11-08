6657d7f097f39d01e3597bbfbe0227f26f072b6a
mazen <109769999+mazens6@users.noreply.github.com>
2022-11-08 21:47:03 +0200

    Add files via upload

---
+++ loding.js
@@ -0,0 +1,3 @@
+setTimeout(()=>{
+    window.location.href = "sup.html"
+},400);
---
+++ main.js
@@ -0,0 +1,12 @@
+function test (){
+    setTimeout(()=>{
+        window.location.href = "lodin.html"
+    },100)
+}
+
+function text (){
+    setTimeout(()=>{
+        window.location.href = "lodin.html"
+    },100)
+}
+
